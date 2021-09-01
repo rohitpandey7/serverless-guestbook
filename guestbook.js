8444aeef1dc297fa29694b9d594777f1ddfe1825
rohitpandey7 <87520163+rohitpandey7@users.noreply.github.com>
2021-09-01 11:46:39 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://5f93b482.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
